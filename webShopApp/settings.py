e108786086b31502cceb93a2ef6cd7885f3810aa
Mbaye Jacques DIOP <mbaye.diop@xegit.com>
2021-03-03 15:56:56 +0000

    static file

--- webShopApp/settings.py
+++ webShopApp/settings.py
@@ -135,7 +135,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -244,7 +244,7 @@ USE_TZ = True
 
 STATIC_URL = '/static_src/'
 
-#STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_src')]
 
